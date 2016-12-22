6ed6114b5286bc48a68f2193f50a274159e5a1b9
David C. Conner <david.conner@cnu.edu>
2016-12-21 20:13:59 -0500

    comment out code that sets tf_prefix twice (#526)

--- gazebo_plugins/src/gazebo_ros_camera_utils.cpp
+++ gazebo_plugins/src/gazebo_ros_camera_utils.cpp
@@ -119,7 +119,7 @@ void GazeboRosCameraUtils::Load(sensors::SensorPtr _parent,
 
   std::stringstream ss;
   this->robot_namespace_ =  GetRobotNamespace(_parent, _sdf, "Camera");
-  
+
   this->image_topic_name_ = "image_raw";
   if (this->sdf->HasElement("imageTopicName"))
     this->image_topic_name_ = this->sdf->Get<std::string>("imageTopicName");
@@ -278,15 +278,18 @@ void GazeboRosCameraUtils::LoadThread()
           *this->rosnode_, this->camera_name_));
 
   // resolve tf prefix
-  std::string key;
-  if(this->rosnode_->searchParam("tf_prefix", key)){
-    std::string prefix;
-    this->rosnode_->getParam(key, prefix);
-    this->frame_name_ = tf::resolve(prefix, this->frame_name_);
-  }
+  // ------- handle with tf::getPrefixParam below --------
+  // github issue #526
+  //std::string key;
+  //if(this->rosnode_->searchParam("tf_prefix", key)){
+  //  std::string prefix;
+  //  this->rosnode_->getParam(key, prefix);
+  //  this->frame_name_ = tf::resolve(prefix, this->frame_name_);
+  //}
+  // ---------------------------------------------------------------
 
   this->itnode_ = new image_transport::ImageTransport(*this->rosnode_);
-  
+
   // resolve tf prefix
   this->tf_prefix_ = tf::getPrefixParam(*this->rosnode_);
   if(this->tf_prefix_.empty()) {
@@ -297,8 +300,8 @@ void GazeboRosCameraUtils::LoadThread()
 
   ROS_INFO("Camera Plugin (ns = %s)  <tf_prefix_>, set to \"%s\"",
              this->robot_namespace_.c_str(), this->tf_prefix_.c_str());
-  
-  
+
+
   if (!this->camera_name_.empty())
   {
     dyn_srv_ =
